05f614eb366c18e3d98619341eed01a59dbb21e3
Ivan <ivanaryvidyan57@gmail.com>
2022-08-18 22:49:32 +0700

    Finish creating Home page and sponsor page

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
+const copyright = document.querySelector(".copyright");
 // const bignav_ctn = document.querySelector(".big-nav__container");
 
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav--visible");
 });
+
+copyright.innerHTML = new Date().getFullYear();
