ef97e12adf1b549fce3bf64f9d8865a506946add
Ivan <ivanaryvidyan57@gmail.com>
2022-08-19 18:57:47 +0700

    completed everything

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 const copyright = document.querySelector(".copyright");
-// const bignav_ctn = document.querySelector(".big-nav__container");
 
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav--visible");
